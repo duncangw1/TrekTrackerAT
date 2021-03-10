f81ec951ea0f514a57fb81ea8df444a8dcf30ca6
WD40 <69653174+duncangw1@users.noreply.github.com>
2021-03-09 20:36:42 -0500

    Updated connection.js to fix terminal deprecation warnings.

--- server/server.js
+++ server/server.js
@@ -1,50 +1,52 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const morgan = require('morgan');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const dbConnection = require('./config/connection');
-const passport = require('./config/passport');
-const path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const dbConnection = require("./config/connection");
+const passport = require("./config/passport");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Middlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/', express.static(path.join(__dirname, '../client/build')));
+app.use("/", express.static(path.join(__dirname, "../client/build")));
 
 // Passport
 app.use(passport.initialize());
 app.use(passport.session()); // will call the deserializeUser
-app.use(session({
-  secret: process.env.AUTH_SECRET,
-  store: new MongoStore({ mongooseConnection: dbConnection }),
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: process.env.AUTH_SECRET,
+    store: new MongoStore({ mongooseConnection: dbConnection }),
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/'))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/"));
 });
 
 // Add Auth and API routes
-app.use('/auth', require('./routes/authRoutes'));
-app.use('/api', require('./routes/apiRoutes'));
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/api", require("./routes/apiRoutes"));
 
 // If no routes are hit, send the React app
-app.use(function(req, res) {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+app.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
 
 // Error handler
-app.use(function(err, req, res, next) {
-	console.error(err.stack);
-	res.status(500);
-})
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500);
+});
 
 app.listen(PORT, () => {
-	console.log(`App listening on PORT: ${PORT}`);
+  console.log(`App listening on PORT: ${PORT}`);
 });
