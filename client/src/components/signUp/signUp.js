2ce809f7295d6558d5e1c1db745d17a383d2493b
WD40 <69653174+duncangw1@users.noreply.github.com>
2021-03-11 18:27:07 -0500

    Changed signUp and signIn icons.

--- client/src/components/signUp/signUp.js
+++ client/src/components/signUp/signUp.js
@@ -6,7 +6,7 @@ import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import InsertPhotoIcon from "@material-ui/icons/InsertPhoto";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
@@ -52,7 +52,7 @@ export default function SignUp() {
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
+          <InsertPhotoIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
           Sign up
