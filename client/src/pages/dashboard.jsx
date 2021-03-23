8983b346006936f9dbe85e13f3c401c7bcc6a294
WD40 <69653174+duncangw1@users.noreply.github.com>
2021-03-22 22:24:55 -0400

    Removed extra link to main page.

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Dashboard from "../components/dashboard/dashboard";
 
 const MainPage = () => {
   return (
     <div>
       <Dashboard />
-      <Link to="/">Main Page</Link>
     </div>
   );
 };
